17296f2dbb0694988256d9b2f0137442801f09c7
leitesbianca <143121694+leitesbianca@users.noreply.github.com>
2024-05-24 14:41:18 -0300

    primer commit

--- model/RegistroModel.php
+++ model/RegistroModel.php
@@ -2,6 +2,14 @@
 
 class RegistroModel
 {
+    private $database;
+    public function __construct ($database){
+        $this->database = $database;
+    }
 
+    public function add($nombreCompleto, $email, $fechaDeNacimiento, $genero, $pais, $ciudad,$nombreDeUsuario, $password, $fotoDePerfil){
+        $sql = "INSERT INTO usuario (nombreCompleto, email, fechaDeNacimiento, genero, pais, ciudad, nombreDeUsuairo, password, fotoDePerfil) 
+        VALUES ($nombreCompleto, $email, $fechaDeNacimiento, $genero, $pais, $ciudad, $nombreDeUsuario, $password, $fotoDePerfil)";
+    }
 
 }
