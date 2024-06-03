fddc3e901b4fc18c791340f96295eb088bbb7743
DomineYanina <domineyanina.elizabeth@gmail.com>
2024-06-02 23:47:12 -0300

    -

--- controller/UserController.php
+++ controller/UserController.php
@@ -2,7 +2,6 @@
 
 class UserController
 {
-
     private $model;
     private $presenter;
 
@@ -14,9 +13,7 @@ class UserController
     public function obtenerDetallesDelUsuario(){
         if(isset($_SESSION['id'])){
             $userId = $_SESSION['id'];
-
             $usuario = $this->model->getUserDetails($userId);
-
             $this->presenter->render("view/perfilPropioView.mustache", [$usuario => $usuario]);
         }
     }
@@ -24,11 +21,23 @@ class UserController
     public function obtenerInformacionLobby(){
         if(isset($_SESSION['id'])){
             $userId = $_SESSION['id'];
-
             $usuario = $this->model->getUserDetailsLobby($userId);
-
             $this->presenter->render("view/perfilPropioView.mustache", [$usuario => $usuario]);
         }
     }
 
+    public function redirigirNuevaPartida()
+    {
+        $this->presenter->render("view/jugarPartidaInicio.mustache");
+    }
+
+    public function redirigirDatosUsuario()
+    {
+        $this->presenter->render("view/perfilPropioView.mustache");
+    }
+
+    public function redirigirRanking()
+    {
+        $this->presenter->render("view/verRanking.mustache");
+    }
 }
