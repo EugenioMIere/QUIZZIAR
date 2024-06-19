ac3fbaf2b7a99dc12198f6a75bda9e0a47ceb88c
leitesbianca <143121694+leitesbianca@users.noreply.github.com>
2024-06-18 22:08:27 -0300

    Editar perfil, menú de editor y sugerir pregunta

--- model/UserModel.php
+++ model/UserModel.php
@@ -27,4 +27,44 @@ class UserModel
         $this->database->execute($query);
     }
 
+    public function sugerirPregunta($preg){
+        $query = "INSERT INTO preguntas_sugeridas(pregunta) VALUES ('$preg')";
+        return $this->database->execute($query);
+    }
+
+    public function editarNombreComp($nc, $id){
+        $query = "UPDATE usuario SET nombreCompleto = '$nc' WHERE id = '$id'";
+        return $this->database->execute($query);
+    }
+
+    public function editarEmail($e, $id){
+        $query = "UPDATE usuario SET email = '$e' WHERE id = '$id'";
+        return $this->database->execute($query);
+    }
+
+    public function editarPais($pais, $id){
+        $query = "UPDATE usuario SET pais = '$pais' WHERE id = '$id'";
+        return $this->database->execute($query);
+    }
+
+    public function editarCiudad($c, $id){
+        $query = "UPDATE usuario SET ciudad = '$c' WHERE id = '$id'";
+        return $this->database->execute($query);
+    }
+
+    public function editarNombreUsuario($nu, $id){
+        $query = "UPDATE usuario SET nombreDeUsuario = '$nu' WHERE id = '$id'";
+        return $this->database->execute($query);
+    }
+
+    public function editarGenero($g, $id){
+        $query = "UPDATE usuario SET genero = '$g' WHERE id = '$id'";
+        return $this->database->execute($query);
+    }
+
+    public function editarFoto($f, $id){
+        $query = "UPDATE usuario SET fotoDePerfil = '$f' WHERE id = '$id'";
+        return $this->database->execute($query);
+    }
+
 }
