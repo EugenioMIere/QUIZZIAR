250867709fc8e0d6e2b42548a052897ef254759b
Eugenio <emierearrua@alumno.unlam.edu.ar>
2024-06-20 22:20:48 -0300

    dificultad de preguntas y nivel de usuario

--- model/PreguntaModel.php
+++ model/PreguntaModel.php
@@ -12,11 +12,32 @@ class PreguntaModel
     public function getPreguntas($id_usuario){
 
         $preguntaPreguntada = [];
+        $dificultad = $this->dificultadSegunNivel($id_usuario);
 
         while (empty($preguntaPreguntada)) {
 
-            $query = "SELECT * FROM preguntas ORDER BY RAND() LIMIT 1";
+            $query = "SELECT * FROM preguntas WHERE dificultad = '$dificultad' ORDER BY RAND() LIMIT 1";
             $result = $this->database->query($query);
+
+            if (empty($result)){
+                $dificultad = 'Facil';
+                $query = "SELECT * FROM preguntas WHERE dificultad = '$dificultad' ORDER BY RAND() LIMIT 1";
+                $result = $this->database->query($query);
+
+                if (empty($result)){
+                    $dificultad = 'Medio';
+                    $query = "SELECT * FROM preguntas WHERE dificultad = '$dificultad' ORDER BY RAND() LIMIT 1";
+                    $result = $this->database->query($query);
+
+                        if (empty($result)){
+                            $dificultad = 'Dificil';
+                        $query = "SELECT * FROM preguntas WHERE dificultad = '$dificultad' ORDER BY RAND() LIMIT 1";
+                        $result = $this->database->query($query);
+
+                        }
+
+                }
+            }
             $idPregunta = $result[0]['id'];
 
             $sql = "SELECT pp.pregunta_id, p.usuario_id
@@ -62,6 +83,7 @@ class PreguntaModel
     public function setRespuestaPartida($usuario_id,$respuesta,$idPregunta,$idRespuestas){
 
         $partida = $this->getUltimaPartidaUsario($usuario_id);
+        $this->updateDificultad($idPregunta);
 
 
         $correctas = $partida[0]['correctas'];
@@ -114,5 +136,135 @@ class PreguntaModel
         $sql = "INSERT INTO preguntas_reportadas(pregunta_reportada) VALUES ('$id')";
         return $this->database->execute($sql);
     }
+
+
+    public function dificultadSegunNivel($id_usuario){
+        $nivelJugador = $this->getNivelDeJugador($id_usuario);
+
+        $dificultadPregunta = null;
+
+        if ($nivelJugador == 'avanzado') {
+            $dificultadPregunta = 'Dificil';
+        } elseif ($nivelJugador == 'intermedio') {
+            $dificultadPregunta = 'Medio';
+        } else {
+            $dificultadPregunta = 'Facil';
+        }
+
+        return $dificultadPregunta;
+
+
+    }
+    public function getNivelDeJugador($id_usuario)
+    {
+        $correctas = $this->cantidadDePreguntasCorrectasPorUsuario($id_usuario);
+        $intentos = $this->cantidadDePreguntasPorUsuario($id_usuario);
+
+        $nivelJugador = null;
+
+        $porcentajeCorrectas = ($correctas / $intentos) * 100;
+
+        if ($porcentajeCorrectas >= 70) {
+            $nivelJugador = 'avanzado';
+        } elseif ($porcentajeCorrectas >= 30) {
+            $nivelJugador = 'intermedio';
+        } else {
+            $nivelJugador = 'principiante';
+        }
+
+        if ($intentos <= 10) {
+            $nivelJugador = 'principiante';
+        }
+
+        return $nivelJugador;
+
+
+    }
+
+    private function cantidadDePreguntasCorrectasPorUsuario($id_usuario){
+
+        $sql = "SELECT COUNT(*) AS correcta
+                FROM partidas_preguntas pp
+                INNER JOIN partidas p ON pp.partida_id = p.id
+                WHERE p.usuario_id = '$id_usuario' AND correcta = '1'";
+
+        $resultado = $this->database->query($sql);
+
+        return $resultado[0]["correcta"];
+
+    }
+    private function cantidadDePreguntasPorUsuario($id_usuario){
+        $sql = "SELECT COUNT(*) AS correcta
+                FROM partidas_preguntas pp
+                INNER JOIN partidas p ON pp.partida_id = p.id
+                WHERE p.usuario_id = '$id_usuario'";
+
+        $resultado = $this->database->query($sql);
+
+        return $resultado[0]["correcta"];
+
+    }
+
+    private function vecesPreguntada($pregunta_id){
+        $sql = "SELECT COUNT(*) AS pregunta_id
+                FROM partidas_preguntas
+                WHERE pregunta_id = '$pregunta_id'";
+
+        $resultado = $this->database->query($sql);
+
+        return $resultado[0]["pregunta_id"];
+
+    }
+
+    private function vecesAcertada($pregunta_id){
+        $sql = "SELECT COUNT(*) AS pregunta_id
+                FROM partidas_preguntas
+                WHERE pregunta_id = '$pregunta_id' AND correcta = '1'";
+
+        $resultado = $this->database->query($sql);
+
+        return $resultado[0]["pregunta_id"];
+
+    }
+
+    private function calcularDificultad($pregunta_id)
+    {
+        $vecesPreguntada = $this->vecesPreguntada($pregunta_id);
+        $vecesAcertada = $this->vecesAcertada($pregunta_id);
+
+        $ratioAciertos = $vecesAcertada / $vecesPreguntada * 100;
+
+        $dificultadPregunta = null;
+
+        if ($ratioAciertos >= 70) {
+            $dificultadPregunta = 'Facil';
+        } elseif ($ratioAciertos < 30) {
+            $dificultadPregunta = 'Dificil';
+        } else {
+            $dificultadPregunta = 'Medio';
+        }
+
+
+        return $dificultadPregunta;
+    }
+
+    public function updateDificultad($pregunta_id){
+        $nuevaDificultad = $this->calcularDificultad($pregunta_id);
+
+        $sql = "UPDATE preguntas SET dificultad = '$nuevaDificultad' WHERE id = '$pregunta_id';";
+
+        $this->database->execute($sql);
+    }
+
+    public function getDificultad($pregunta_id){
+        $sql = "SELECT dificultad FROM preguntas WHERE id = '$pregunta_id';";
+
+        $resultado = $this->database->query($sql);
+
+        return $resultado[0]["dificultad"];
+    }
+
+
+
 
 }
