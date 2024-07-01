e37cbcea9dfd038ff5f5ae0968cecd150597bb5a
leitesbianca <143121694+leitesbianca@users.noreply.github.com>
2024-06-30 22:57:11 -0300

    Perfil de otro usuario completo

--- model/UserModel.php
+++ model/UserModel.php
@@ -86,5 +86,34 @@ class UserModel
         return $this->database->execute($sql);
     }
 
+    public function getInfoJuego($idUsuario){
+        $sql = "SELECT SUM(p.correctas) as correctas, SUM(p.incorrectas) as incorrectas, COUNT(DISTINCT p.id) as cantidad
+                FROM partidas p JOIN usuario u 
+                ON p.usuario_id = u.id";
+        return $this->database->execute($sql);
+    }
+
+    public function getNivelJugador($idUsuario){
+        $sql = "SELECT p.usuario_id, COUNT(DISTINCT p.id) AS partidas_jugadas,
+       SUM(CASE WHEN pp.correcta = 1 THEN 1 ELSE 0 END) AS total_correctas,
+        COUNT(pp.pregunta_id) AS total_preguntas,
+        CASE 
+        WHEN COUNT(DISTINCT p.id) = 0 THEN 0
+        ELSE SUM(CASE WHEN pp.correcta = 1 THEN 1 ELSE 0 END) * 1.0 / COUNT(DISTINCT p.id)
+        END AS promedio_correctas_por_partida,
+        CASE 
+        WHEN COUNT(DISTINCT p.id) = 0 THEN 0
+        ELSE COUNT(pp.pregunta_id) * 1.0 / COUNT(DISTINCT p.id)
+        END AS promedio_preguntas_por_partida
+        FROM partidas p INNER JOIN partidas_preguntas pp ON pp.partida_id = p.id
+        WHERE p.usuario_id = '$idUsuario'";
+
+        return $this->database->query($sql);
+    }
+
+    public function getEdad($idUsuario){
+        $sql = "SELECT TIMESTAMPDIFF(YEAR, fechaDeNacimiento, CURDATE()) AS edad FROM usuario WHERE id = '$idUsuario'";
+        return $this->database->execute($sql);
+    }
 
 }
