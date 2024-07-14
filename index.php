d75db056ae503ef1514b97c3857424a88b9e21dd
leitesbianca <143121694+leitesbianca@users.noreply.github.com>
2024-07-13 22:37:47 -0300

    Permitir acceso según roles

--- index.php
+++ index.php
@@ -6,8 +6,41 @@ $router = Configuration::getRouter();
 
 if (isset($_SESSION['rol'])) {
 
-    $controller = isset($_GET["controller"]) ? $_GET["controller"] : "" ;
-    $action = isset($_GET["action"]) ? $_GET["action"] : "";
+    $rol = $_SESSION['rol'];
+    $controllersValidos = ["admin", "editor", "login", "pregunta", "ranking", "registro", "user"];
+    $controller = "login";
+
+    if (isset($_GET['controller'])){
+        $controller = $_GET['controller'];
+
+        if ($_SESSION['rol'] === 'editor'){
+            switch ($controller){
+                case "admin":
+                case "pregunta":
+                case "ranking":
+                case "user":
+                    header("Location: /login");
+                    break;
+            }
+        } elseif ($_SESSION['rol'] === 'user'){
+            switch ($controller){
+                case "admin":
+                case "editor":
+                    header("Location: /login");
+                    break;
+            }
+        } elseif ($_SESSION['rol'] === 'admin'){
+            switch ($controller){
+                case "user":
+                case "editor":
+                case "pregunta":
+                case "ranking":
+                header("Location: /login");
+                    break;
+            }
+        }
+        $action = isset($_GET['action']) ? $_GET['action'] : " ";
+    }
 
 }elseif($_GET["controller"]=="login"||$_GET["controller"]=="registro" ) {
 
